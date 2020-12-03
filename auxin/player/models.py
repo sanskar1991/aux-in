a763e5685eac287627043becd6520d1d643fbc1a
sanskar1991 <sanskarj123@gmail.com>
2020-12-03 17:08:21 +0530

    Model Creation

--- auxin/player/models.py
+++ auxin/player/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Albums(models.Model):
+    """
+    Model for storing Albums
+    """
+
+    album_name = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Songs(models.Model):
+    """
+    Model for storing all the songs corresponding to an album
+    """
+
+    album_name = models.ForeignKey(Albums, on_delete=models.CASCADE)
+    song_name = models.CharField(max_length=200)
